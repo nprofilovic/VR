32ee6f7a706e041e8f8c256666270a874e44843f
nprofilovic <nprofilovic@gmail.com>
2018-01-10 10:50:54 +0100

    Update index file

--- index.vr.js
+++ index.vr.js
@@ -7,12 +7,37 @@ import {
   View,
 } from 'react-vr';
 import Canvas from './components/Canvas';
+import UI from './components/UI';
+
+const Config = [
+  {
+    key: 0,
+    imageSrc: 'reactconf_00.jpg',
+    buttonImageSrc: 'button-00.png',
+  },
+  {
+    key: 1,
+    imageSrc: 'reactconf_01.jpg',
+    buttonImageSrc: 'button-01.png',
+  },
+  {
+    key: 2,
+    imageSrc: 'reactconf_02.jpg',
+    buttonImageSrc: 'button-02.png',
+  },
+  {
+    key: 3,
+    imageSrc: 'reactconf_03.jpg',
+    buttonImageSrc: 'button-03.png',
+  }
+];
+
 export default class HelloVR extends React.Component {
   constructor(props){
     super(props);
 
     this.state = {
-      src: './chess-world.jpg'
+      src: 'reactconf_00.jpg'
     }
   }
 
@@ -21,6 +46,9 @@ export default class HelloVR extends React.Component {
     return (
       <View>
         <Canvas src={this.state.src}/>
+        <UI buttonConfig={Config} onClick={(key) =>{
+          this.setState({src: Config[key].imageSrc});
+        }} />
       </View>
     );
   }
